62276849c7c9eee97c1b21c9e8358c47ae503bde
sonde756 <edsonrafael756@gmail.com>
2023-05-20 17:58:41 -0300

    Seller entity class

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,12 @@
+package model.dao;
+
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    List<Seller> findAll();
+}
